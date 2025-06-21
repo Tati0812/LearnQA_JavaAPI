334117a36f44311bb5c31a4aa3fd0e88a5d4d596
Tati0812 <t.ermakova08@gmail.com>
2025-06-22 00:13:13 +0300

    Ex10

---
+++ src/test/java/Ex10.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Ex10 {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"This row more then 15 symbols","123456789012345"})
+    public void testLength(String text){
+
+        assertTrue(text.length() > 15, "Length is shorter or equal 15");
+    }
+
+
+
+}
